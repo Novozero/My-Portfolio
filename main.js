bfcf5aed28231c213f3f147b63136652a80b6d0d
Marco <m.greiner313@gmail.com>
2024-09-25 15:34:56 +0200

    Made some modifications. -Added a responsive CSS Folder -Improved HTML - Added responsive design to the header(arrow button for smaller devices)

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const techStackImg = document.querySelector(".tech-stack-img")
-const techStackGrid = document.getElementById("toggleContainer")
+const techStackImg = document.querySelector(".tech-stack-img");
+const techStackGrid = document.getElementById("toggleContainer");
+let isOpen = false;
 
-const toggle = (e) =>{
-    e.classList.toggle("tech-stack-grid-container")
-}
+const toggle = () => {
+  if (!isOpen) {
+    techStackGrid.classList.add("show");
+    techStackGrid.style.opacity = "1";
+  } else {
+    techStackGrid.style.opacity = "0";
+    setTimeout(() => techStackGrid.classList.remove("show"), 1000);
+  }
+  isOpen = !isOpen;
+};
 
-techStackImg.addEventListener("click", ()=>{
-    toggle(techStackGrid)
+techStackImg.addEventListener("click", toggle);
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  const slideButton = document.getElementById("slideButton")
+  const header = document.querySelector('header')
+  const arrow = document.getElementById('arrow')
+
+  slideButton.addEventListener('click', ()=>{
+      header.classList.toggle("open")
+      slideButton.classList.toggle("open")
+      arrow.classList.toggle("open")
+  })
 })
