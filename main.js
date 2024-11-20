4f7a0e5d4f2e9110ce839803f6cea8e227e1dcc5
Marco <m.greiner313@gmail.com>
2024-11-20 14:51:56 +0100

    Removed Javascript for open and close the TechStack Added background Added and improved responsive design Added project section

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-const techStackImg = document.querySelector(".tech-stack-img");
-const techStackGrid = document.getElementById("toggleContainer");
-let isOpen = false;
-
-const toggle = () => {
-  if (!isOpen) {
-    techStackGrid.classList.add("show");
-    techStackGrid.style.opacity = "1";
-  } else {
-    techStackGrid.style.opacity = "0";
-    setTimeout(() => techStackGrid.classList.remove("show"), 1000);
-  }
-  isOpen = !isOpen;
-};
-
-techStackImg.addEventListener("click", toggle);
-
-
 document.addEventListener('DOMContentLoaded', function(){
   const slideButton = document.getElementById("slideButton")
   const header = document.querySelector('header')
